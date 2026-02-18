479c3f140bf61deadd993ca44308e367676eaf0f
hyo4402 <nguyenminhhieu4402@gmail.com>
2026-02-18 14:56:14 +0700

    g

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // GitHub Pages base URL is vital. Use './' for relative paths.
-  base: './', 
-});
